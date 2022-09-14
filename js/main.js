ed0735dfa0d265cf3d6f4179905ee060892f91a2
anshul <anshulsharmaa.76@gmail.com>
2022-09-14 22:28:11 +0530

    Portfolio

---
+++ js/main.js
@@ -0,0 +1,23 @@
+//Navigation bar effects on scroll
+
+window.addEventListener("scroll" ,function(){
+    const header =document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+//Portfolio section - Modal
+
+//Our clients - Swiper
+
+//Website dark/light theme
+
+//Scroll to top button
+
+//Navigation menu items active on page scroll
+
+//Responsive navigation menu toggle
+
+//Scroll reveal animations
+//Common reveal options to create reveal animations
+
+//Target elements, and specify options to create reveal animations
