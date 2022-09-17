b72abf2bc434b35f5efff8c8f7844ffe6e0df851
Jhala26 <anshulsharmaa.76@gmail.com>
2022-09-17 13:46:07 +0530

    project

--- js/main.js
+++ js/main.js
@@ -6,8 +6,44 @@ window.addEventListener("scroll" ,function(){
 });
 
 //Portfolio section - Modal
+const serviceModals = document.querySelectorAll(".service-modal");
+const learnMoreBtn = document.querySelectorAll(".learn-more-btn");
+const modalCloseBtn = document.querySelectorAll(".modal-close-btn");
+
+var modal = function(modalClick){
+    serviceModals[modalClick].classList.add("active");
+}
+
+learnMoreBtn.forEach((learnMoreBtn, i) => {
+    learnMoreBtn.addEventListener("click", () => {
+        modal(i);
+    });
+});
+
+modalCloseBtn.forEach((modalCloseBtn) => {
+    modalCloseBtn.addEventListener("click", () => {
+        serviceModals.forEach((modalView) => {
+            modalView.classList.remove("active");
+        });
+    });
+});
+
+
 
 //Our clients - Swiper
+var swiper = new Swiper(".client-swiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 
 //Website dark/light theme
 
@@ -16,6 +52,24 @@ window.addEventListener("scroll" ,function(){
 //Navigation menu items active on page scroll
 
 //Responsive navigation menu toggle
+const menuBtn =document.querySelector(".nav-menu-btn");
+const closeBtn =document.querySelector(".nav-close-btn");
+const navigation =document.querySelector(".navigation");
+const navItem =document.querySelectorAll(".nav-items a");
+
+menuBtn.addEventListener("click", () =>{
+    navigation.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () =>{
+    navigation.classList.remove("active");
+});
+
+navItem.forEach((navItem)=>{
+    navItem.addEventListener("click", ()=>{
+        navigation.classList.remove("active");
+    })
+})
 
 //Scroll reveal animations
 //Common reveal options to create reveal animations
