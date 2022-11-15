5b96b51a97a8f71b6e073f37be0d990068745880
anshul <anshulsharmaa.76@gmail.com>
2022-11-15 18:07:35 +0530

    portfolio

--- js/main.js
+++ js/main.js
@@ -31,19 +31,19 @@ modalCloseBtn.forEach((modalCloseBtn) => {
 
 
 //Our clients - Swiper
-var swiper = new Swiper(".client-swiper", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+// var swiper = new Swiper(".client-swiper", {
+//     slidesPerView: 1,
+//     spaceBetween: 30,
+//     loop: true,
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+//   });
 
 //Website dark/light theme
 
